4b9ee8451d9b6c8505188b238d1c09f63bdab26e
cmjimerson1s <cmjimerson1s@gmail.com>
2023-01-11 09:16:35 +0000

    Add function to list the users names as strings

--- run.py
+++ run.py
@@ -14,14 +14,16 @@ SHEET = GSPREAD_CLIENT.open('timekeeper')
 
 NAMES = SHEET.get_worksheet(0)
 
-def get_names():
+
+def start_program():
     """
-    Collects the names from the Employee sheet and displays
+    Initilaizes the program requesting the employee to view/edit"
     """
     names = SHEET.get_worksheet(0)
     data = names.col_values(1)
-    print(data)
-
+    list = ", ".join(data)
+    print('Please choose the employee whose data you wish to view.')
+    print(f'The current employees are {list}.')
 
 def add_new_sheet():
     """
@@ -33,6 +35,10 @@ def add_new_sheet():
 def add_new_name():
     NAMES.update('A4', 'Eddy')
 
-add_new_name()
-get_names()
-add_new_sheet()
+
+
+
+start_program()
+# add_new_name()
+# get_names()
+# add_new_sheet()
