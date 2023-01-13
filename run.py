654d14bff0e44f337011c60011d0e7ca886ec11d
cmjimerson1s <cmjimerson1s@gmail.com>
2023-01-13 12:57:18 +0000

    Update update_cells_function with working code to push user submitted data to sheet

--- run.py
+++ run.py
@@ -40,30 +40,6 @@ def start_program():
             break
 
 
-def validate_employee_name(names, choice):
-    """
-    Validates that the chosen name by the user matches current employees
-    """
-    if choice in names:
-        print("Correct")    
-    else:
-        print('Wrong')
-        return False
-    return True
-
-
-def validate_menu_num(menu, choice):
-    """
-    Validates that the chosen menu number by the user matches current menu
-    """
-    if choice in menu:
-        print("Correct")    
-    else:
-        print('Wrong')
-        return False
-    return True
-
-
 def main_menu(name_choice):
     """
     Initializes the main menu for the user to choose what to do with selected employee
@@ -78,12 +54,12 @@ def main_menu(name_choice):
         print("4. Return to Employee Select\n")
         
         while True:
-                # This validates the input, ensureing inability to enter a blank input
-                main_menu_choice = input("Menu Number: ")
-                if not main_menu_choice:
-                    print('Please enter valid menu number.')
-                else:
-                    break
+            # This validates the input, ensureing inability to enter a blank input
+            main_menu_choice = input("Menu Number: ")
+            if not main_menu_choice:
+                print('Please enter valid menu number.')
+            else:
+                break
 
         if validate_menu_num(menu_options, main_menu_choice):
             menu_selector(name_choice, main_menu_choice)
@@ -117,60 +93,97 @@ def menu_two(name_choice):
     Collects and displays all the current time keeping data for selected employee
     Allows user to select an existing data, clockin, and clockout to edit
     """
-    print(f"Menu 2: Currently viewing {name_choice}'s Hours\n")
-    names = SHEET.get_worksheet(0)
-    data = names.col_values(1)
-    x = data.index(name_choice)
-    hours = SHEET.get_worksheet(x)
-    info = pd.DataFrame(hours.get_all_records())
-    print(info)
-    edit_choice = (int(input('What entry would you like to edit(Use number in far left column): '))) + 2
+    while True:
+        print(f"Menu 2: Currently viewing {name_choice}'s Hours\n")
+        names = SHEET.get_worksheet(0)
+        data = names.col_values(1)
+        x = data.index(name_choice)
+        hours = SHEET.get_worksheet(x)
+        info = pd.DataFrame(hours.get_all_records())   
+        list_length = hours.get_all_values()
+        print(info)
 
-    if edit_choice == 3:
-        display_choice = hours.row_values(edit_choice)
-        print(display_choice)
-        get_edit_row(edit_choice, hours)
+        while True:
+            # This validates the input, ensureing inability to enter a blank input
+            string_edit_choice = str(input('What entry would you like to edit(Use number in far left column): '))
+            if not string_edit_choice:
+                print('Please enter valid menu number.')
+            else:
+                break
+
+        if validate_menue_two_edit_choice(string_edit_choice, list_length):
+            edit_choice = (int(string_edit_choice))+2
+            display_choice = hours.row_values(edit_choice)
+            print(display_choice)
+            get_edit_row(edit_choice, hours, name_choice)
 
 
-    
 def menu_selector(name_choice, main_menu_choice):
     """
     Runs to direct user towards the apporiate menu of their selection
     """
     if main_menu_choice == "1":
         menu_one(name_choice)
-    else:
+    elif main_menu_choice == "2":
         menu_two(name_choice)
 
-def get_edit_row(row_choice, sheet_choice):
+
+def get_edit_row(row_choice, sheet_choice, name_choice):
     row_updated = str(row_choice)
-    
     new_date = str(input("Enter Date: "))
     new_clockin = str(input("Enter Clock-in: "))
     new_clockout = str(input("Enter Clock-out: "))
 
-    sheet_choice.update(f'A{row_updated}', new_clockout)
+    update_cells_hours(new_date, new_clockin, new_clockout, row_updated, sheet_choice, name_choice)
 
 
-# def update_cells_hours(date, clockin, clockou, row, sheet):
-#     sheet.update(f'A{row_updated}
+def update_cells_hours(date, clockin, clockout, row, sheet, name_choice):
+    sheet.update(f'A{row}', date)
+    sheet.update(f'B{row}', clockin)
+    sheet.update(f'C{row}', clockout)
+    print("Updating date and hours...\n")
+    print("Returning to Menu 2. Please wait...\n")
+    menu_two(name_choice)
 
+#Below this comment sits all the validation functions for the above menu functions
+def validate_employee_name(names, choice):
+    """
+    Validates that the chosen name by the user matches current employees
+    """
+    if choice in names:
+        print("Correct")    
+    else:
+        print('Wrong')
+        return False
+    return True
+
+
+def validate_menu_num(menu, choice):
+    """
+    Validates that the chosen menu number by the user matches current menu
+    """
+    if choice in menu:
+        print("Correct")    
+    else:
+        print('Wrong')
+        return False
+    return True
+
+def validate_menue_two_edit_choice(edit_choice, list_of_hours):
+    choice_check = (int(edit_choice) + 1)
+    if choice_check < len(list_of_hours):
+        print("Valid entry")
+    else:
+        print("Invalid entry. Try again.")
+        return False
+    return True 
 
 
 
 
 start_program()
 
-# class TimeEdit:
 
-#     def __init__(self, date, clockin, clockout):
-#         self.date = date
-#         self.clockin = clockin
-#         self.clockout = clockout
-
-# new_date = input("Enter new date: ")
-# new_clockin = input("Enter new clock-in: ")
-# new_clockout = input("Enter new clock-out: ")
 
 
 
