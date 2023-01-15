0cc7626767aaf848725e2830892728c5bb983e02
cmjimerson1s <cmjimerson1s@gmail.com>
2023-01-15 18:35:06 +0000

    Add basic code to unit test

---
+++ unittest.py
@@ -0,0 +1,6 @@
+import unittest
+from run.py import time_calculation
+
+class TestTimeCalculation(unittest.TestCase):
+    def test_negative(self)
+    # Tests the conversion of negative minutes to positive
